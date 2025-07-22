8792456ca23495f4f4afaadf3ae774824489b50b
OjasavGupta73 <ojasavgupta73@gmail.com>
2025-07-22 22:01:41 +0530

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,7 @@ public class Main {
         } catch (InvalidInputException e) {
             System.out.println("Error: " + e.getMessage());
         }
+        System.out.println("Create a PR for this");
 
     }
 
